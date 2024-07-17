9b8860f09912e0070d8475696158d2188fd9f971
Nafis Ahamed <nafisahamed14@gmail.com>
2024-07-17 18:28:38 +0600

    admin history add

--- src/components/dashboard/admindashboard/Admin.jsx
+++ src/components/dashboard/admindashboard/Admin.jsx
@@ -1,11 +1,49 @@
-
-
+import useAxiospublic from "../../hooks/useAxiospublic";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../useAuth";
 
 const Admin = () => {
+     const axiosPublic = useAxiospublic()
+     const {users} = useAuth()
+     const fetchUserData = async () => {
+        try {
+          const res = await axiosPublic.get(`/transfer`);
+          return res.data;
+        } catch (err) {
+          console.error(err);
+          throw err;
+        }
+      };
+      const { data: userx = [],error, isLoading} = useQuery({
+        queryKey: ["menu",users?.email],
+        queryFn: fetchUserData
+      });
+      if (isLoading) return <p>Loading...</p>;
+      if (error) return <p>Error: {error.message}</p>;
     return (
-        <div>
-            <h2 className="text-2xl">Admin</h2>
-        </div>
+        <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>to</th>
+              <th>Method</th>
+              <th>Money</th>
+              <th>charge</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userx?.map((it) => (
+              <tr key={it._id}>
+                <td>{it.from}</td>
+                <td>{it.method}</td>
+                <td>{it.money}</td>
+                <td>{it.charge}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
 };
 
