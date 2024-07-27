586217dd12263ee079544b8a0636b6f6b43e4d64
Nafis Ahamed <nafisahamed14@gmail.com>
2024-07-27 20:51:18 +0600

    code done

--- src/components/dashboard/admindashboard/Adminpermit.jsx
+++ src/components/dashboard/admindashboard/Adminpermit.jsx
@@ -1,74 +1,97 @@
 import useAxiospublic from "../../hooks/useAxiospublic";
 import useAuth from "../../useAuth";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 const Adminpermit = () => {
-    const {users} = useAuth()
-    const axiosPublic = useAxiospublic()
-    const fetchUserData = async () => {
-        try {
-          const res = await axiosPublic.get(`/admin`);
-          return res.data;
-        } catch (err) {
-          console.error(err);
-          throw err;
-        }
-      };
-      const { data: userx = [],error, isLoading,refetch} = useQuery({
-        queryKey: ["menu",users?.email],
-        queryFn: fetchUserData
+  const { users } = useAuth();
+  const axiosPublic = useAxiospublic();
+  const [count, setCount] = useState("all");
+  let role = "all";
+  const fetchUserData = async () => {
+    try {
+      const res = await axiosPublic.get(`/admin/${role}`);
+      return res.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  };
+  const {
+    data: userx = [],
+    error,
+    isLoading,
+    refetch
+  } = useQuery({
+    queryKey: ["menu", users?.email],
+    queryFn: fetchUserData
+  });
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const handleMakeAdmin = (user) => {};
+
+  const handleMakeSurveyor = (user) => {
+    axiosPublic.patch(`/admined/${user.email}`, {}).then(() => {
+      Swal.fire({
+        icon: "success",
+        title: "yaa...",
+        text: "you are agent",
+        footer: "Add 10000 taka user account"
       });
-      if (isLoading) return <p>Loading...</p>;
-      if (error) return <p>Error: {error.message}</p>;
-      console.log(userx)
+      refetch();
+    });
+  };
 
-      const handleMakeAdmin = (user) => {
-       
-      };
-
-      const handleMakeSurveyor = (user) => {
-        axiosPublic.patch(`/admined/${user.email}`,{})
-        .then(()=>{
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosPublic.delete(`/feedback/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
             Swal.fire({
-                icon: "success",
-                title: "yaa...",
-                text: "you are agent",
-                footer: 'Add 10000 taka user account'
-            });
-            refetch()
-        })
-      };
-    
-
-    const handleDeleteUser = (user) => {
-        Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-          if (result.isConfirmed) {
-            axiosPublic.delete(`/feedback/${user._id}`).then((res) => {
-              if (res.data.deletedCount > 0) {
-                refetch();
-                Swal.fire({
-                  title: "Deleted!",
-                  text: "Your file has been deleted.",
-                  icon: "success"
-                });
-              }
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
             });
           }
         });
-      }; 
+      }
+    });
+  };
 
+  const Search = () => {
+    role = count;
+    refetch()
+  };
 
-    return (
-        <div className="overflow-x-auto">
+  return (
+    <div>
+      <div className="flex justify-center mx-auto p-4">
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered input-info w-full max-w-xs rounded-3xl"
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button
+          onClick={Search}
+          className="btn btn-active btn-primary ml-2 rounded-2xl"
+        >
+          Search
+        </button>
+      </div>
+
+      <div className="overflow-x-auto">
         <table className="table table-zebra w-full">
           {/* head */}
           <thead>
@@ -86,27 +109,25 @@ const Adminpermit = () => {
                 <th>{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
+                <td>{user.role === "admin" && <p>I am Admin</p>}</td>
+                <td>{user.role === "agent" && <p>I am Agent</p>}</td>
                 <td>
-                  {user.role === "admin" && 
-                    <p>I am Admin</p>
-                  }
-                </td>
-                <td>
-                  {user.role === "agent" && 
-                    <p>I am Agent</p>
-                  }
-                </td>
-                <td>
-                   {
-                     user.role == "user" && <div className="space-x-2 space-y-2">
-                     <button onClick={()=>handleMakeSurveyor(user)} className="btn btn-primary"> make Agent</button>
-                     </div>
-            
-                   }
+                  {user.role == "user" && (
+                    <div className="space-x-2 space-y-2">
+                      <button
+                        onClick={() => handleMakeSurveyor(user)}
+                        className="btn btn-primary"
+                      >
+                        {" "}
+                        make Agent
+                      </button>
+                    </div>
+                  )}
                 </td>
                 <td>
                   <button
-                    className="btn btn-ghost btn-lg"  onClick={() => handleDeleteUser(user)}
+                    className="btn btn-ghost btn-lg"
+                    onClick={() => handleDeleteUser(user)}
                   >
                     <FaTrashAlt className="text-red-600"></FaTrashAlt>
                   </button>
@@ -116,7 +137,8 @@ const Adminpermit = () => {
           </tbody>
         </table>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Adminpermit;
--- src/components/hooks/useAxiospublic.jsx
+++ src/components/hooks/useAxiospublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "https://finally-deploy.vercel.app"
+  baseURL:"https://finally-deploy.vercel.app"
 });
 
 const useAxiospublic = () => {
