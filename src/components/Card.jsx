596e4912d353d70be701f28d8cd7bdb50044f70b
Nafis Ahamed <nafisahamed14@gmail.com>
2024-08-15 01:19:11 +0600

    pagination start

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,20 +2,25 @@
 import React, { useEffect, useState } from 'react';
 
 const Card = () => {
-    const [count, setCount] = useState(null);
+    const [itemPerPages, setItemPerPages] = useState(null);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [count,setCount] = useState(null)
 
     useEffect(() => {
         fetch('http://localhost:5000/productsCount')
         .then(res =>res.json())
-            .then(data => setCount(data.count))
+            .then(data => {
+                setItemPerPages(data.count)
+                setCount(data.count)
+            })
             .catch(error => {
                 console.error('There was a problem with the fetch operation:', error);
             });
     }, []);
-    console.log(count)
+    
     return (
         <div>
-            <h2>{count}</h2>
+            
         </div>
     );
 };
