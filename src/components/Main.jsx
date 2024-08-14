13104391c7ae149999ee36d3417984055331dceb
Nafis Ahamed <nafisahamed14@gmail.com>
2024-08-14 23:16:56 +0600

    footer add

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 
 
@@ -9,6 +10,7 @@ const Main = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
